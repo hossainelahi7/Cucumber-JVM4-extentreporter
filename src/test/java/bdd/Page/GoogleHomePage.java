20f65f051d4d202a9b713324e0f2e518bd7a63b0
he94627 <hossain.elahi@thehartford.com>
2019-03-28 14:44:21 -0400

    code clean up

--- src/test/java/bdd/Page/GoogleHomePage.java
+++ src/test/java/bdd/Page/GoogleHomePage.java
@@ -3,12 +3,7 @@ package bdd.Page;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-import webDriver.Util.Driver;
 
 public class GoogleHomePage extends Page{
 	private String url = "https://www.google.com/";
